04d7931c3dee404700fb582f7986b14d34455d61
Michael Partin <mike.partin@gmail.com>
2019-03-14 12:35:37 -0400

    template "DR-redis" & Cleanup

--- ansible/roles/codebase_api/files/api/apiv1/urls.py
+++ ansible/roles/codebase_api/files/api/apiv1/urls.py
@@ -3,10 +3,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-  #url(r'^LNEx/register', views.LNExRegister, name='LNExRegister'),
   url(r'^LNEx/initZone', views.LNExInit, name='LNExInit'),
   url(r'^LNEx/destroyZone', views.LNExDestroy, name='LNExDestroy'),
-  #url(r'^LNEx/extract', views.LNExExtract, name='LNExExtract'),
   url(r'^LNEx/results', views.LNExResults, name='LNExResults'),
   url(r'^LNEx/geoInfo', views.LNExGeoInfo, name='LNExGeoInfo'),
   url(r'^LNEx/bulkExtract', views.LNExBulkExtract, name='LNExBulkExtract'),
